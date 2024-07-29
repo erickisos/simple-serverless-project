94eaaabd5c965259d13c30e7b586796a4ae53788
Erick G. Islas-Osuna <erickisos653@gmail.com>
2024-07-29 17:15:36 -0600

    fix: modifica README y agrega handler de ejemplo (#73)

---
+++ handlers/webhook.py
@@ -0,0 +1,16 @@
+import os
+
+from aws_xray_sdk.core import patch_all, xray_recorder  # type: ignore
+
+from ssp.models.context import Context
+from ssp.ports.http_inputs import webhook
+from ssp.schema.aws import LambdaApiRequest, LambdaApiResponse
+
+patch_all()
+
+# Lo que sea que necesites inyectar en tu aplicación
+config: Context = dict()
+
+
+def webhook_handler(event: LambdaApiRequest, _) -> LambdaApiResponse:
+    return webhook(event, config)
