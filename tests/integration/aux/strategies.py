306e5412000bb44508ad130799e3e498f72dac91
Erick G. Islas-Osuna <erickisos653@gmail.com>
2024-08-01 13:52:47 -0600

    feat: agrega prueba de integracion para el webhook handler (#74)

---
+++ tests/integration/aux/strategies.py
@@ -0,0 +1,17 @@
+import json
+
+from hypothesis.strategies import composite, from_type
+from pydantic.json import pydantic_encoder
+
+from ssp.schema.aws import LambdaApiRequest
+
+
+@composite
+def event_with_json_body(draw, body):
+    """Add any Pydantic dataclass as body of ApiEvent"""
+    api_event = draw(from_type(LambdaApiRequest))
+    api_body = draw(from_type(body))
+    return {
+        **api_event,
+        'body': json.dumps(api_body, default=pydantic_encoder),
+    }
